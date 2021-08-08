82f6762d7ca2f9932e295847486e812e40d2d4bb
honoruru <psanchez@hawaiiusafcu.com>
2021-08-07 19:45:06 -1000

    Final Challenge Commit

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,53 @@
+-- DELIVERABLE 1
+-- Create retirement_titles
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   t.title,
+	   t.from_date,
+	   t.to_date
+INTO retirement_titles_rev
+FROM employees AS e
+INNER JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no) 	
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles_rev
+FROM retirement_titles_rev
+ORDER BY emp_no, to_date DESC;
+
+--Retiring Titles
+SELECT COUNT(title) AS "count", title
+INTO retiring_titles_rev
+FROM unique_titles_rev
+GROUP BY title
+ORDER BY "count" DESC;
+
+-- DELIVERABLE 2
+
+--Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+JOIN titles AS t
+	ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+ 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
